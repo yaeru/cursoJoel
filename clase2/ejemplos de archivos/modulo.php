b7b4e5648ceddf1978a693c8b0a8e7432e0c0bd3
Yael Duckwen <yaelduckwen@gmail.com>
2023-05-27 19:21:25 -0300

    Los archivos de la clase 2

---
+++ clase2/ejemplos de archivos/modulo.php
@@ -0,0 +1,4 @@
+<h1>Este es un doc PHP</h1>
+<?php
+phpinfo();
+?>
---
+++ clase2/ejemplos de archivos/script.js
@@ -0,0 +1,3 @@
+var input = document.getElementById("tarea");
+var boton = document.getElementById("boton");
+var tareas =document.querySelector("ul");
